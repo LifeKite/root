dd734fc3e5c06dc40c70d39c0e04e2ad5e672f30
Rich <rwnagle3@gmail.com>
2012-06-11 21:28:46 -0400

    Update main screen

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,47 +5,25 @@
 //= require jquery-ui
 
 $(document).ready(function(){
-	$('#kiteContainerUl li:first').before($('#kiteContainerUl li:last'));
 	
-	//Get the number of items currently in the list
-	var kiteCount = $('#kiteContainerUl').children("li").length;
-	
+	function setCenterStage(image) {
+		$('#showcase').css('background', 'url(' + $(image).attr('src') + ') no-repeat center');
+		//$('#showcase').empty();
+		var end = $('#moreinfo a').attr('href').lastIndexOf('/');
+		var y = end == 0 ? $('#moreinfo a').attr('href') : $('#moreinfo a').attr('href').slice(0, end); 
+		y = y + '/' + $(image).attr('name');
+		$('#moreinfo a').attr('href', y);
 		
-	//Get the number of kites that can fit on the container
-	// at its current width
-	var maxKites = Math.floor($('#kites').width() / 210);
+		$('#kiteDesc').text($(image).attr('alt'));
+	}
 	
-	var kites = Math.min(kiteCount, maxKites);
+	$('#kitesContainer').jcarousel({vertical:true, scroll: 1, wrap: "circular"});
 	
-	var actualWidth = kites * (210);
-	
-	//Set the width of the kitesinner to a multiple of the
-	// width of an item coresponding to the number of items 
-	// that can completely fit in the current window
-	$('#kitesInner').width(actualWidth);
-	
-	$('#left_scroll').click(function(){
-		var itemWidth = $('#kiteContainerUl li').outerWidth() + 20;
-		var leftIndent = parseInt($('#kiteContainerUl').css('left')) - itemWidth;
-		$('#kiteContainerUl:not(:animated)').animate({'left' : leftIndent},500, function(){
-					$('#kiteContainerUl li:last').after($('#kiteContainerUl li:first'));
-					$('#kiteContainerUl').css({'left':'-220px'});
-				});
-	});
-	
-	$('#right_scroll').click(function(){
-		var itemWidth = $('#kiteContainerUl li').outerWidth() + 20;
-		var leftIndent = parseInt($('#kiteContainerUl').css('left')) + itemWidth;
-		$('#kiteContainerUl:not(:animated)').animate({'left' : leftIndent},500, function(){
-					$('#kiteContainerUl li:first').before($('#kiteContainerUl li:last'));
-					$('#kiteContainerUl').css({'left' : '-220px'});
-				});
-	});
-	$('#kiteContainerUl li').click(function(){
-		$('#showcase').empty();
-		$(this).clone().appendTo('#showcase');
+	$('#kitesContainer li img').click(function(){
+		setCenterStage(this);	
 	});
 	
 	//Automatically select the first
-	$('#kiteContainerUl li:first').clone().appendTo('#showcase');
+	setCenterStage($('#kitesContainer li:first img'));
+
 });
