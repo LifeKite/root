2d87fd4db854c9d62f36271bb3cfa455d1e5a1f3
Rich <rwnagle3@gmail.com>
2012-08-22 22:17:15 -0400

    Minor updates and first version mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,11 @@
 class NotificationMailer < ActionMailer::Base
+  default :from => 'lifekite-info@gmail.com'
   def notifymail(notification)
     
     @user = User.find(notification[:user_id]);
     @subject = 'Kite-comment notification'
     @recipients = @user.email
-    @from = 'lifekite-info@gmail.com'
+    
     @message = notification[:message]
     @receiver = @user.username
     @kitepath = notification[:link]
