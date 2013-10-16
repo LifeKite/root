c0b552e49d12b84299232f88717c110cc4a9a86c
rich <rich@electro.(none)>
2013-10-15 21:19:03 -0400

    Fixes to FB login and initial pass at invites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -117,7 +117,7 @@ class UsersController < ApplicationController
   # A very simple invitation system
   def invite
     
-    UserMailer.invite_email(@user, params[:email]).deliver
+    UserMailer.invite_email(current_user, params[:email]).deliver
     redirect_to(kites_path, :notify => "Your invitation has been sent")
   end
   
