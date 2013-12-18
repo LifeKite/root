22c601e6c398c14f142988230afc94fe9f62db4a
rich <rich@electro.(none)>
2013-12-18 16:27:22 -0500

    Test password reset tool

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   
    
   def after_sign_in_path_for(resource)
-
+    
     sign_in_url = new_session_path(resource, :only_path => false)
     sign_up_url = new_registration_path(resource, :only_path => false)  
        
-    if (request.referer == sign_in_url || request.referer == sign_up_url )
+    if (request.referer == sign_in_url || request.referer == sign_up_url || request.referer.nil?)
         kites_path
     else
         request.referer
