1e797ba1cb7edb314ad8519dd50b32c181fb77f3
rich <rich@electro.(none)>
2013-12-18 19:56:22 -0500

    Additional logging

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Rlifekite
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
+    config.logger = Logger.new(STDOUT)
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password, :password_confirmation]
     config.assets.initialize_on_precompile = false
