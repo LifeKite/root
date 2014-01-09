e377318c1bf17727733d317443b31fec7ce75533
Rich <rwnagle3@gmail.com>
2014-01-09 12:22:40 -0500

    Allow fb users to modify their profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def safe_user_edit_path(resource, *args, &block)
+    options = args.extract_options!
+    if resource.provider == "facebook"
+      args.delete("url")
+      args.delete("as") 
+    end
+    form_for(resource, *args, &block)
+  end
 end
