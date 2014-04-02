262f43a8a1844bb18068b9b022006a345b89f35a
Mateusz Konikowski <mateusz@freeportmetrics.com>
2014-04-02 13:35:28 +0200

    refs #12542: add rvm settings, upgrade rails, do not use aws for local development

--- app/models/kite_post.rb
+++ app/models/kite_post.rb
@@ -1,26 +1,20 @@
 class KitePost < ActiveRecord::Base
   attr_accessible :text, :image
   belongs_to :kite
-   
+
   require 'aws/s3'
-  
-  @@aws_bucket_id = ENV['AMAZON_BUCKET_ID']
-  @@access_key_id = ENV['AMAZON_ACCESS_KEY_ID']
-  @@secret_access_key = ENV['AMAZON_SECRET_ACCESS_KEY']
-  
-  has_attached_file :image,
+
+  has_attached_file :image, {
     :styles => { :medium => {:geometry => "300x300>", :format => :png}, :thumb => {:geometry => "215x215>", :format => :png} },
-        :default_style => :original, 
-        :storage => :s3,
-        :s3_credentials => { :access_key_id => @@access_key_id, :secret_access_key => @@secret_access_key},
-        :bucket => @@aws_bucket_id
-       
+    :default_style => :original
+  }
+
   scope :associated_kite, joins(:kite)
-        
+
   scope :search_by_tag, lambda { |tag|
         (tag ? where(["kite_posts.tag LIKE ?", '%#'+ tag + '%'])  : {})
-  } 
-         
+  }
+
   def FormattedCreateDate
     return self.created_at.strftime("%B %d, %Y")
   end
