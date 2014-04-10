297c3d8263b5ef686a2f5876c1665d9a66a07153
Mateusz Konikowski <mateusz@freeportmetrics.com>
2014-04-10 18:26:33 +0200

    refs #12698, #12697, #12695: simple load on scroll

--- app/assets/javascripts/kites.js
+++ app/assets/javascripts/kites.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+  jQuery(function() {
+    if ($('.pagination').length) {
+      $(window).scroll(function() {
+        var url;
+        url = $('.pagination .next_page').attr('href');
+        if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+          $('.pagination').text("Fetching more kites...");
+          return $.getScript(url);
+        }
+      });
+      return $(window).scroll();
+    }
+  });
+
   $('.side-kites').tabs();
 });
