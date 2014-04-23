293294a2a2cc09d68a4438f9ac5f81e649043c2f
Piotr Delinowski <piotr@freeportmetrics.com>
2014-04-23 16:00:31 +0200

    refs #12705: second approach to resizing kite image on scroll - resizing height instead of width

--- app/assets/javascripts/kite.js
+++ app/assets/javascripts/kite.js
@@ -1,40 +1,42 @@
 $(document).ready(function () {
+
   if ($(".detail-kite-img").size() > 0) {
+
+    var img = $(".detail-kite-img img");
+    img.data('height', img.height());
+    img.data('posTop', img.position().top);
+    img.height(img.height()).width('auto');
+
     $(window).scroll(function () {
 
       if (window.resizing) return;
-
       window.resizing = true;
 
-      //Window Math
       var scrollTop = $(window).scrollTop(),
           docHeight = $(document).height(),
           windowHeight = $(window).height();
+
       var scrollPercent = (scrollTop / (docHeight - windowHeight)) * 100;
-
-//      scrollPercent = Math.ceil(scrollPercent);
-
-      if (scrollPercent < 0) {
-        scrollPercent = 0;
-      }
-      if (scrollPercent > 100) {
-        scrollPercent = 100;
+      if (scrollPercent >= 100) {
+        window.resizing = false;
+        return;
       }
 
-//      console.log(scrollPercent);
+      var minHeight = 300;
+      var originalHeight = parseInt(img.data('height'), 10);
 
-      var width = (100 - scrollPercent);
-      if (width < 30) {
-        width = 30;
+      var height = originalHeight - scrollTop;
+      if (height < minHeight) {
+        height = minHeight;
+      }
+      if (height > originalHeight) {
+        height = originalHeight;
       }
 
-      if (scrollPercent == 0 || width == 30) {
-        $(".detail-kite-img img").animate({width: width + '%'}, 'fast');
-      } else {
-        $(".detail-kite-img img").width(width + '%');
-      }
+      img.height(height);
 
       window.resizing = false;
+
     }).trigger('scroll');
   }
 });
